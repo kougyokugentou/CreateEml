19c10f5f41a03d58a01a7e60ec68a44a2782b918
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-04-29 17:04:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateEml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var createEmail = new createEmail();
+
+            string eml = createEmail.CreateEml("brapp@enron.com", "agent47@hammersrus.com", "Payment for hammers", 
+            "I've sent you half the payment for those hammers I ordered, please proceed to execute manufacturing at your discretion. Here are my payment details:\n\r" +
+            "PAYMENT FROM Bitcoin Address: n4kD8bt29izZUuxZVthXscSbTdQPoE5wX9 ; PAYMENT TO Bitcoin Address: myeyDMQeqeDXbbhJhtA7NSvHA8vkzaoSrr ; " +
+            "TRANSACTION ID: 9a2468889936b87b8d64211e1d2a36afebff6c73eaa8fa4a849cfe099076e723 ; AMOUNT: 0.47 BTC\n\r\n\r - Bill"
+            ,null,
+            @"C:\devl\443-Files\Custodian Source\Documents\Outlook\");
+        }
+    }
+}
---
+++ createEmail.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+
+namespace CreateEml
+{
+    public class createEmail
+    {
+        /// <summary>
+        /// Creates an eml file to be delivered with the default mail client
+        /// </summary>
+        /// <param name="from">address that sends the email</param>
+        /// <param name="to">address to which send the email</param>
+        /// <param name="subject">subject</param>
+        /// <param name="body">body</param>
+        /// <param name="attachments">list of attachments</param>
+        /// <param name="destFolder">destination folder to store the file</param>
+        /// <returns>The complete path of the newely generated EML file</returns>
+
+        public static string CreateEml(string from,
+                                       string to,
+                                       string subject,
+                                       string body,
+                                       IEnumerable<Attachment> attachments,
+                                       string destFolder)
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(from);
+                mail.To.Add(to);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                if (attachments != null)
+                    foreach (var attachment in attachments)
+                        mail.Attachments.Add(attachment);
+
+                SmtpClient smtpClient = new SmtpClient
+                {
+                    PickupDirectoryLocation = destFolder,
+                    DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory
+                };
+                smtpClient.Send(mail);
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(destFolder);
+            FileInfo file = dir.GetFiles("*.eml", SearchOption.TopDirectoryOnly).OrderByDescending(m => m.CreationTimeUtc).First();
+            return file.FullName;
+        }
+    }
+}
